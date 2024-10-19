bdfa93980d2e434de8ca516382fb86a431364072
paritosh-Shukla24 <paritosh.fastlane.com>
2024-10-19 11:24:15 +0530

    fast api

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app=FastAPI()
+#Decorator path operation
+
+
+## create a Request we use Pydantic BaseModel
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=9000)
